2555ee14485854719d4d4884764620794f32f4b9
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-12 20:16:08 +0100

    upload

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -1,11 +1,35 @@
 const { request, response } = require('express');
+const jwt = require('jsonwebtoken');
 
 const validateJWT = (req = request, res = response, next) => {
 
-    const jwt = request.header('jwt');
+    const jwtUser = request.header('jwt');
 
+    if (!jwtUser) {
 
-    next();
+        return res.status(401).json({
+            ok: false,
+            msg: 'No JWT in headers'
+        });
+
+    }
+
+    try {
+
+        const { uid } = jwt.verify(jwtUser, process.env.JWT_SECRET);
+
+        req.uid = uid;
+
+        next();
+        
+    } catch (error) {
+        
+        return res.status(401).json({
+            ok: false,
+            msg: 'JWT not valid'
+        });
+
+    }
 
 }
 
